5aba20947f5b9e2f4e67432983ed7db579337083
Marie Schmidt <mschmidt@kenzan.com>
2016-02-05 16:07:49 -0700

    Changed the initial conditions of the table results picker to be set by the function instead of manually - a la Justin

--- JS/controllers/TableResultsHeaderCtrl.js
+++ JS/controllers/TableResultsHeaderCtrl.js
@@ -5,18 +5,6 @@
 
         var vm = this;
 
-        vm.resultsCategory = 'individual';
-        vm.resultsDesired = null;
-        var promise = dropDownData.getData(vm.resultsCategory);
-        promise.then(
-            function (callback) {
-                vm.dropDownArray = callback.data;
-            }, function (error) {
-                console.log(error)
-            });
-
-        vm.tableHeading = vm.resultsCategory + ' Results';
-
         //set category, null out results desired, and generate dropdown menu
         vm.setResultsCategory = function (value) {
             vm.resultsCategory = value;
@@ -31,5 +19,9 @@
             vm.tableHeading = value + ' Results';
         };
 
+        vm.setResultsCategory('individual');
+
+        vm.tableHeading = vm.resultsCategory + ' Results';
+        
     }]);
 })();
